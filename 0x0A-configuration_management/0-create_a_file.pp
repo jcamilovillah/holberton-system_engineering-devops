541be8f9d5a32c332151c2782d84560e6bc3009d
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-07 16:43:51 -0500

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 
 file { '/tmp/holberton':
-    ensure   => 'present',
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data',
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
